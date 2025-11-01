fd5f4f95f9f7ab0cfd5656f972590bc46f0ee058
Akshay4718 <akshayanilnaik1@gmail.com>
2025-11-01 16:15:23 +0530

    fix: add redirects for SPA routing on Render

---
+++ frontend/dist/assets/Home-CXp0C4rw.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-B3aSgIEu.js";import{N as r}from"./NoticeBox-B0Yj9OfZ.js";import{N as s}from"./NotificationBox-Bu8gPC7Z.js";function m(){return document.title="CPMS | TPO Dashboard",o.jsx(o.Fragment,{children:o.jsxs("div",{className:"grid grid-cols-2 gap-2 max-sm:grid-cols-1",children:[o.jsx(s,{}),o.jsx(r,{})]})})}export{m as default};
