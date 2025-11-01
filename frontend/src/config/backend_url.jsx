b2ccaf8ca68c7860b9bd3785ea74f1b4976a38a1
Akshay4718 <akshayanilnaik1@gmail.com>
2025-11-01 15:33:27 +0530

    backend url from render added

--- frontend/src/config/backend_url.jsx
+++ frontend/src/config/backend_url.jsx
@@ -1,2 +1,2 @@
-export const BASE_URL = 'http://localhost:4518';
+export const BASE_URL = 'https://major-project-voq9.onrender.com' ||'http://localhost:4518';
 //export const BASE_URL = 'https://cpms-api.vercel.app';
