365d2b2eb5fbfe3b89113fb578902a686d27e109
Akshay4718 <akshayanilnaik1@gmail.com>
2025-11-02 09:19:26 +0530

    b url

--- backend/index.js
+++ backend/index.js
@@ -9,15 +9,7 @@ const __dirname = dirname(__filename);
 
 const app = express();
 
-//
-// app.use(express.static(path.join(__dirname, 'build')));
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// });
 
-
-
-//
 
 
 app.use(express.json());
